943c7cccbcb63c91cd4561bf16580d46dca3aa5e
Ashish S <ashishsawant4u@gmail.com>
2023-01-17 16:07:05 +0530

    basic spring config server

---
+++ src/test/java/com/devex/config/ConfigServerPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devex.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
